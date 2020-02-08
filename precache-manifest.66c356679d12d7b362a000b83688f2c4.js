53289c4340ee6631f9d1e89bbf1fea4acfa5c9d0
DingYau <dingyau@HKT-BTU-Mac.local>
2020-02-08 11:12:08 +0800

    Updates

---
+++ precache-manifest.66c356679d12d7b362a000b83688f2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a2391c1485449880c33fe5996ea0f8",
+    "url": "/myhksariweb/index.html"
+  },
+  {
+    "revision": "6162762a3f27f5121857",
+    "url": "/myhksariweb/static/css/main.c80ad70b.chunk.css"
+  },
+  {
+    "revision": "65adf50fc0cc86af3ba4",
+    "url": "/myhksariweb/static/js/2.4e6336fe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myhksariweb/static/js/2.4e6336fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6162762a3f27f5121857",
+    "url": "/myhksariweb/static/js/main.284dbfa3.chunk.js"
+  },
+  {
+    "revision": "79681f15407821c780fb",
+    "url": "/myhksariweb/static/js/runtime-main.ce814d15.js"
+  }
+]);
