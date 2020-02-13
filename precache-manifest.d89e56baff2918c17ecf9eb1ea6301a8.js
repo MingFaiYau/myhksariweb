f32bd0b5aaca85b590b7586370375aad01359511
DingYau <dingyau@HKT-BTU-Mac.local>
2020-02-13 23:25:16 +0800

    Updates

---
+++ precache-manifest.d89e56baff2918c17ecf9eb1ea6301a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58678a86e2bf360de30afaa18d9e2452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbca7adb479df9a8c8db",
+    "url": "/static/js/2.20ce0bac.chunk.js"
+  },
+  {
+    "revision": "571bb35b60bf9acca7a459440e824b38",
+    "url": "/static/js/2.20ce0bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f095dd043faa3c81977",
+    "url": "/static/js/main.04052062.chunk.js"
+  },
+  {
+    "revision": "ef088c5bbffd71417152",
+    "url": "/static/js/runtime-main.c0017b9d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myhksariweb/precache-manifest.b055e889eb7901919551a0ff449530e8.js"
+  "/precache-manifest.d89e56baff2918c17ecf9eb1ea6301a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myhksariweb/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
